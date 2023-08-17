0a88a811f63503b317d9ebe3fe97497372eaacd7
wirandahidayatama <randahidayatama2000@gmail.com>
2023-08-17 19:43:52 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,7 +20,7 @@
 <body class="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
     <header class="max-w-lg mx-auto">
         <a href="#">
-            <h1 class="text-4xl font-bold text-white text-center">APLIKASI IURANnnnnnn</h1>
+            <h1 class="text-4xl font-bold text-white text-center">APLIKASI IURANnnnnnn tessttt</h1>
         </a>
     </header>
 
